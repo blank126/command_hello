1259db2c8e45961a43b2ecec1df025495c785154
blank126 <donggyu488@gmail.com>
2025-04-11 09:54:10 +0900

    github for git pull

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 // local repository modification
 // github modification
 
-
 print("hello world")
 print("tell your world")
 print("tell his world")
 print("tell her world")
+print("tell my world")
